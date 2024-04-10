94e6868d770f426fb4519e8641153bf3539583a0
Arthur Borges Garcez <arthur_b_garcez@hotmail.com>
2024-04-10 19:32:52 -0300

    Primeiro commit

---
+++ src/main/java/br/univille/projfabsoft01/Projfabsoft01Application.java
@@ -0,0 +1,13 @@
+package br.univille.projfabsoft01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projfabsoft01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projfabsoft01Application.class, args);
+	}
+
+}
